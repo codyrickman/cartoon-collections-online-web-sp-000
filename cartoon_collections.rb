775f779cb37e12283387917a702004e2f9420f59
Cody Rickman <cody@creativemindstudios.com>
2018-11-23 21:24:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,6 @@ def find_the_cheese (cheese_array)# code an argument here
   cheese_array.each do |string|
     if cheese_types.include?(string) == true
       return string
-    else
-      return nil
     end
   end
 
